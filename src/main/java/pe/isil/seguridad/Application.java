c83b3af9df98556d147fa90d55b3359293c5791a
Joaquin <joaquinm683@gmail.com>
2022-09-14 10:38:04 -0500

    Se genera estructura para desarrollar app

---
+++ src/main/java/pe/isil/seguridad/Application.java
@@ -0,0 +1,16 @@
+package pe.isil.seguridad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+
+        System.out.println("Hola Mundo");
+
+    }
+
+}
